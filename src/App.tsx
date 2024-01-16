228ab4235707154116a0da9183018b3d1edf4cd7
Marius STEPHANY <marius.stephany@gmail.com>
2024-01-16 17:14:25 +0100

    :fire: Remove navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { GrFormPrevious } from "react-icons/gr";
 import Device from "./Computer";
 import ComputerScene from "./components/ComputerScene";
 import ExpandableCard from "./components/ExpandableCard";
-import Navbar from "./components/Navbar";
 import PresentationSection from "./components/PresentationSection";
 
 function App() {
@@ -32,11 +31,10 @@ function App() {
         <section
           className={`w-screen h-screen grid ${
             isViewOpened
-              ? "grid grid-cols-1 grid-rows-[40px_1fr_0fr] gap-0 bg-[#041F1E] text-[#F7DBA7]"
-              : "grid grid-cols-2 md:grid-cols-3 grid-rows-[40px_1fr_1fr] gap-4 bg-[#F7DBA7] text-[#041F1E]"
-          } md:grid-rows-[60px_1fr] md:grid-rows transition-all p-4`}
+              ? "grid grid-cols-1 grid-rows-[1fr_0fr] gap-0 bg-[#041F1E] text-[#F7DBA7]"
+              : "grid grid-cols-2 md:grid-cols-3 grid-rows-[1fr_1fr] gap-4 bg-[#F7DBA7] text-[#041F1E]"
+          } md:grid-rows-1 transition-all p-4`}
         >
-          <Navbar />
           {!isViewOpened && (
             <div className="flex flex-col justify-evenly items-center col-span-2 md:col-span-1">
               <PresentationSection />
