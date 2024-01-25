29afed34d3d862bec3b8fc55586ed15f2c6f7366
Marius STEPHANY <marius.stephany@gmail.com>
2024-01-25 10:29:58 +0100

    :sparkles: Add hover gradient on Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
 export default function Button({ link, text, icon }: ButtonProps) {
   return (
     <a
-      className="flex justify-center items-center gap-3 w-full sm:w-auto rounded-lg hover:bg-gradient-to-l bg-gradient-to-r from-secondary to-secondary-variant px-2 sm:px-6 py-2 sm:py-3"
+      className="flex justify-center items-center gap-3 w-full sm:w-auto rounded-lg hover:opacity-80 hover:bg-[100%_0] transition-all duration-700 bg-[length:300%_100%] bg-gradient-to-r from-secondary via-secondary-variant to-secondary px-2 sm:px-6 py-2 sm:py-3"
       href={link}
       target="_blank"
     >
